26a598feef08d0e10b2e8a02009e407a19ff9981
Daniel Blake <S3910924@student.rmit.edu.au>
2021-09-29 21:54:07 +0800

    Much nicer feel to the GET function

--- experiment/Daniel/app.js
+++ experiment/Daniel/app.js
@@ -12,39 +12,32 @@ button.onclick = () => getList();
 // important when using fetch(), although you don't necessarily
 // need to understand why at this stage
 async function getList() {
-    // create a variable and make your fetch call. be sure to prefix it
-    // with "await". when the response arrives, it will become the value
-    // of this variable
-    const response = await fetch("http://scrimpr.com/api/groupMembers", {
-            // when calling fetch(), the first parameter is the endpoint (URL) to
-            // send the request to. the optional second parameter is an object
-            // containing headers - the Access-Control-Allow-Origin one shown 
-            // below is important
-        "Access-Control-Allow-Origin": "*"
-    }) // fetch().then() <---- then() is a function that you define for when the
-       // fetch process finally returns with a response, in order to tell it what
-       // you would like to do next. use this generic pattern to return the server's
-       // response into the value of our "response" variable
+    const response = await fetch("http://localhost:3000/api/groupMembers", {
+        "Access-Control-Allow-Origin": "*"})
     .then(data => {
-        return data.json();
+        return data.json()
     })
-
-    // now that we know our response variable contains the response from the server,
-    // we can display it on the webpage. be sure to use JSON stringify to turn the
-    // raw JSON into a normal string
-    div.innerText = "RESPONSE:\n" + JSON.stringify(response);
+    div.innerText = "";
+    for(i=0; i<response.length; i++) {
+        let rawData = JSON.stringify(response[i])
+        let teamMember = JSON.parse(rawData)
+        div.innerText +=teamMember.id+".  "+ teamMember.name+" is on team "+ teamMember.team+"\n";
+        console.log(teamMember.name);
+    };
+    
+   
 }
 //New function that posts what we put into the boxes
 async function poststuff() {
     const name = document.getElementById("name");
     const team = document.getElementById("team");
     const object = { name:name.value, team: team.value };
-    const response = await fetch('http://scrimpr.com/api/groupMembers', {
+    const response = await fetch('http://localhost:3000/api/groupMembers', {
     method: 'POST',
     body: JSON.stringify(object),
     headers: {
         'Content-Type': 'application/json'
     }
     });
-// ...
+getList();
 }
