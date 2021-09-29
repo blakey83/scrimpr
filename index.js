6224a57cd654a16d1cbd659ad05629a56ef6f024
Daniel Blake <S3910924@student.rmit.edu.au>
2021-09-29 08:33:10 +0800

    changing stuff around

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('staticstuff'));
 
 const groupMembers = [
     {id:1, name:'Jennifer', team:'Backend'},
