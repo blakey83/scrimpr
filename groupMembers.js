7f60e071b7cec38f17626bed8459680e6b96af4e
blakey83 <80328500+blakey83@users.noreply.github.com>
2021-10-02 17:36:52 +0800

    Add files via upload

---
+++ groupMembers.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.router();
+
+
+const groupMembers = [
+    {id:1, name:'Jennifer', team:'Backend'},
+    {id:2, name:'Mark', team:'Frontend'},
+    {id:3, name:'Erin', team:'Frontend'},
+    {id:4, name:'Daniel', team:'Backend'},
+    {id:5, name:'Jordan', team:'Backend'},
+    {id:6, name: 'Harrison', team:'Frontend'},
+];
+
+
+router.get('/', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(groupMembers);
+});
+
+router.get('/:name', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const people = groupMembers.find(c=> c.name === req.params.name);
+    if (!people) return res.status(404).send('That person was not found');
+    res.send(people);
+});
+
+router.post('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const result = validatepeople(req.body);
+    const { error } = validatepeople(req.body);
+    if (error) return res.status(400).send(result.error.details[0].message);
+
+    const people = {
+        id: groupMembers.length +1,
+        name: req.body.name,
+        team: req.body.team     
+    };
+
+    groupMembers.push(people);
+    res.send(people);
+});
+
+function validatepeople(course) {
+    const schema = {
+        name: Joi.string().min(3).required(),
+        team: Joi.string().required().valid('Backend','Frontend')
+    };
+
+    return Joi.validate(course, schema);
+};
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,60 +1,12 @@
 const Joi = require('joi');
 const express = require('express');
+const groupMembers = require('./Routes/groupMembers');
 const app = express();
 var cors = require('cors')
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('staticstuff'));
-
-const groupMembers = [
-    {id:1, name:'Jennifer', team:'Backend'},
-    {id:2, name:'Mark', team:'Frontend'},
-    {id:3, name:'Erin', team:'Frontend'},
-    {id:4, name:'Daniel', team:'Backend'},
-    {id:5, name:'Jordan', team:'Backend'},
-    {id:6, name:'"ladyhat" Harrison', team:'Frontend'},
-];
-
-app.get('/fetch', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'fetch/fetch.html'));
-   });
-
-app.get('/api/groupMembers', (req, res) =>{
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.send(groupMembers);
-});
-
-app.get('/api/groupMembers/:name', (req, res) =>{
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    const people = groupMembers.find(c=> c.name === req.params.name);
-    if (!people) return res.status(404).send('That person was not found');
-    res.send(people);
-});
-
-app.post('/api/groupMembers', (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    const result = validatepeople(req.body);
-    const { error } = validatepeople(req.body);
-    if (error) return res.status(400).send(result.error.details[0].message);
-
-    const people = {
-        id: groupMembers.length +1,
-        name: req.body.name,
-        team: req.body.team     
-    };
-
-    groupMembers.push(people);
-    res.send(people);
-});
-
-function validatepeople(course) {
-    const schema = {
-        name: Joi.string().min(3).required(),
-        team: Joi.string().required().valid('Backend','Frontend')
-    };
-
-    return Joi.validate(course, schema);
-};
+app.use('/api/groupMembers', groupMembers);
 
 app.listen(3000, () => console.log('listening on port 3000...'));
