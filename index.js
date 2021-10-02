1ad45db5ed16d6568daca8f57a050f5dc9f76714
Daniel Blake <S3910924@student.rmit.edu.au>
2021-10-02 18:33:00 +0800

    forgot to save index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const express = require('express');
-const groupMembers = require('./Routes/groupMembers');
+const groupMembers = require('./routes/groupMembers');
 const app = express();
 var cors = require('cors')
 
