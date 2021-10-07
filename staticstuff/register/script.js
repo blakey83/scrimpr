630a26db815cca241beb850f27874f7b0bd39c26
Daniel Blake <S3910924@student.rmit.edu.au>
2021-10-07 19:18:40 +0800

    minor changes

--- staticstuff/register/script.js
+++ staticstuff/register/script.js
@@ -1,24 +1,5 @@
 
-
-
-
-// async function attemptLogin() {
-//     const username = usernameField.value;
-//     const password = passwordField.value;
-
-//     // const body = { "username": username, "password": password };
-
-//     // usernameField.value = "";
-//     // passwordField.value = "";
-// }
-
-
 async function addUser(form) {
-    // const firstname = form.firstname.value;
-    // const lastname = form.lastname.value;
-    // const postcode = form.postcode.value;
-    // const Email = form.email.value;
-    // const password = form.password.value;
     const object = { 
         firstname: form.firstname.value, 
         lastname: form.lastname.value,
