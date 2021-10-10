e0b13c5aeb038e4ff3bcba2ee7ee8c01cd054424
decadentpig <jordandevisser@gmail.com>
2021-10-10 21:08:36 +1000

    Lightweight performance improvements on scraper

--- experiment/scraper/index.js
+++ experiment/scraper/index.js
@@ -3,22 +3,11 @@ const path = require("path");
 const puppeteer = require("puppeteer");
 
 (async () => {
-    const browser = await puppeteer.launch({
-        //headless: false,
-        //devtools: true,
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.setJavaScriptEnabled(true);
-    await page.goto("https://shop.coles.com.au/a/national/everything/search/milk");
-
-    let content = await page.content();
-    fs.writeFile("content.html", content, "utf8", (err) => {
-        if (err) throw err;
-        console.log("A file was created: content.html");
-    });
-
-    await page.goto(`file:${  path.join(__dirname, "content.html")  }`);
+    await page.goto("https://shop.coles.com.au/a/national/everything/search/milk?pageNumber=2");
 
     await page.evaluate(() => {
         const productNodes = document.querySelectorAll("div[class='product-main-info']");
