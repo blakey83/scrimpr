d5ad41228a656260f9910cf52af1367362d4215a
decadentpig <jordandevisser@gmail.com>
2021-10-10 20:00:14 +1000

    Removed testing HTML page

--- experiment/scraper/script.js
+++
@@ -1,7 +0,0 @@
-const products = document.querySelectorAll(".product");
-
-for (let i = 0; i < products.length; i++) {
-    console.log("Loop number", i + 0);
-    console.log(products[i].innerText);
-    console.log(products[i].querySelector(".product-name").innerText);
-}
