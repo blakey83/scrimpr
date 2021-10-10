4b63b1d001ef0d6f3edb934c6a632c016ad70295
decadentpig <jordandevisser@gmail.com>
2021-10-10 21:34:26 +1000

    Added working pagination

--- experiment/scraper/index.js
+++ experiment/scraper/index.js
@@ -1,36 +1,53 @@
-const fs = require("fs");
-const path = require("path");
 const puppeteer = require("puppeteer");
 
-(async () => {
+const searchTerm = "cordial";
+
+async function scraper(searchTerm) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.setJavaScriptEnabled(true);
-    await page.goto("https://shop.coles.com.au/a/national/everything/search/milk?pageNumber=2");
 
-    await page.evaluate(() => {
-        const productNodes = document.querySelectorAll("div[class='product-main-info']");
-        let products = [];
+    await page.goto(`https://shop.coles.com.au/a/national/everything/search/${searchTerm}`);
+    const pages = await page.evaluate(() => {
+        return document.querySelector(".pagination").getElementsByTagName("li").length;
+    }).then(result => result);
+    console.log("Detected " + pages + " pages!");
 
-        for (let i = 0; i < productNodes.length; i++) {
-            const productObj = {
-                brand: productNodes[i].querySelector("span[class='product-brand']").innerText,
-                item: productNodes[i].querySelector("span[class='product-name']").innerText,
-                price: productNodes[i].querySelector("span[class='dollar-value']").innerText.concat(
-                    productNodes[i].querySelector("span[class='cent-value']").innerText
-                ),
+    let products = [];
+
+    for (let i = 1; i <= pages; i++) {
+        await page.goto(`https://shop.coles.com.au/a/national/everything/search/${searchTerm}?pageNumber=${i}`);
+
+        await page.evaluate(() => {
+            const productNodes = document.querySelectorAll("div[class='product-main-info']");
+            let productsOnPage = [];
+    
+            for (let j = 0; j < productNodes.length; j++) {
+                const productObj = {
+                    brand: productNodes[j].querySelector("span[class='product-brand']").innerText,
+                    item: productNodes[j].querySelector("span[class='product-name']").innerText,
+                    price: productNodes[j].querySelector("span[class='dollar-value']").innerText.concat(
+                        productNodes[j].querySelector("span[class='cent-value']").innerText
+                    ),
+                }
+    
+                productsOnPage.push(productObj);
             }
+    
+            return productsOnPage;
+        }).then(data => {
+            for (let k = 0; k < data.length; k++) {
+                products.push(data[k]);
+            }
+        });
+    }
 
-            products.push(productObj);
-        }
-
-        return products;
-    }).then(data => {
-        console.log(data);
-    });
+    console.log(products);
 
     await browser.close();
-})();
+}
+
+scraper(searchTerm);
 
 
