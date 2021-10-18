d6067a41cd1ab918afb70bc45d384bf0c56dd716
Jam <s3893768>
2021-10-18 15:03:58 +1000

    rm redundant cors headers in routes

--- src/routes/groupMembers.js
+++ src/routes/groupMembers.js
@@ -4,20 +4,17 @@ const router = express.Router();
 
 
 router.get('/', async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
     const groupMembers = await GroupMember.find().sort('name');
     res.send(groupMembers);
 });
 
 router.get('/:name', async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
     const people = await GroupMember.find({ name: req.params.name });
     if (!people) return res.status(404).send('That person was not found');
     res.send(people);
 });
 
 router.post('/', async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
     const result = validate(req.body);
     const { error } = validate(req.body);
     if (error) return res.status(400).send(result.error.details[0].message);
