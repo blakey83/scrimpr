286e93687c9c093562247ac71fc4ba1f7a2d58fd
decadentpig <jordandevisser@gmail.com>
2021-11-23 11:13:49 +1000

    Included default search term

--- src/routes/render.js
+++ src/routes/render.js
@@ -23,7 +23,7 @@ router.get('/mylists', (_, res) => void res.render('mylists'));
 router.get('/products', (_, res) => void res.render('products'));
 router.get('/register', (_, res) => void res.render('register'));
 router.get('/results', async function(req, res) {
-  res.render('results', { data: await scraper(req.query.search) } );
+  res.render('results', { data: await scraper(req.query.search ?? "groceries") } );
 });
 
 
